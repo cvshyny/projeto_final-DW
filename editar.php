439da257233098a0e427e25a5fe1760053f2e28a
Cv Shyny <camillyvitoria2348real@gmail.com>
2023-05-12 10:28:43 -0300

    Acho que terminei tudo, as areas de edicao e para a edicao de notas esta ao meu agrado

--- editar.php
+++ editar.php
@@ -25,12 +25,12 @@
     
 
     <main class="container">
-        <form class="row d-flex justify-content-center text-center" action="./controller/editar.php" method="post">
-            <div class="col-6 mt-3 ">
+        <form class=" row d-flex justify-content-center text-center" action="./controller/editar.php" method="post">
+            <div class="shadow-lg p-3 mb-5 bg-body rounded  col-12 col-lg-6 mt-3 ">
                 <label class="col-12 fst-italic"  for="Titulo"><h2>Titulo</h2></label>
-                    <input maxlength="20" name="nota"  value="<?php echo $dados["nome_nota"]; ?>" class="col-8 form-floating" id="Titulo" type="text">
+                    <input maxlength="20" name="nota"  value="<?php echo $dados["nome_nota"]; ?>" class="col-8 form-floating form-control" id="Titulo" type="text">
                 <label class="col-12 fst-italic" for="descricao"><h2>Descrição</h2></label>
-                    <textarea maxlength="250" value="<?php echo $dados["nota"]; ?>" class="col-8 " name="descricao" id="descricao" cols="30" rows="10"><?php echo $dados["nota"]; ?></textarea> <br>
+                    <textarea maxlength="250" value="<?php echo $dados["nota"]; ?>" class="col-8 form-control " name="descricao" id="descricao" cols="30" rows="10"><?php echo $dados["nota"]; ?></textarea> <br>
                 <button name="edit" value="<?php echo $_GET["edit"]; ?>" type="submit" style="width: 70px;" class="mt-3 btn btn-primary">Salvar</button> 
             </div>
         </form>
